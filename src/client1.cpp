bb294e46e1f2c956bdd685b2212cebf66e25aa03
Selçuk Öztürk <selcukahmed@gmail.com>
2024-11-28 03:27:18 +0200

    [feat] Add client to connect

--- src/client1.cpp
+++ src/client1.cpp
@@ -1,6 +1,24 @@
+#include <Client.hpp>
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, world!" << std::endl;
+    auto client_result = Client::create();
+    if (client_result.has_error()) {
+        std::cerr << "Failed to create client: " << client_result.error()
+                  << std::endl;
+    }
+
+    auto client = client_result.value();
+    client.add_tcp_port(4001);
+    client.add_tcp_port(4002);
+
+    if (const auto conn_result = client.connect_tcp();
+        conn_result.has_error()) {
+        std::cerr << "Failed to connect to port: " << conn_result.error()
+                  << std::endl;
+    }
+
+    std::cout << "Connection successful" << std::endl;
+
     return 0;
 }
