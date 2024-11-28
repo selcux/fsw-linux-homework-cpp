db86fe15dd4a36c8a54abaf4bb503df8612920ef
Selçuk Öztürk <selcukahmed@gmail.com>
2024-11-28 02:43:23 +0200

    [refactor] Result type as a generic structure

---
+++ include/Result.hpp
@@ -0,0 +1,26 @@
+#ifndef RESULT_HPP
+#define RESULT_HPP
+#include <variant>
+
+#include "ClientError.hpp"
+
+// using ResultVoidErr = std::variant<void, ClientError>;
+// using ResultIntErr = std::variant<int, ClientError>;
+
+template <typename T, typename E = ClientError>
+class Result {
+   public:
+    Result(T value) : value(std::move(value)) {}
+    Result(E error) : error(std::move(error)) {}
+
+    bool has_value() const { return std::holds_alternative<T>(variant); }
+    bool has_error() const { return std::holds_alternative<E>(variant); }
+
+    T& value() const { return std::get<T>(variant); }
+    const E& error() const { return std::get<E>(variant); }
+
+   private:
+    std::variant<T, E> variant;
+};
+
+#endif  // RESULT_HPP
